fb8e5ce42aa5cbd2dbc747694bab1088728e4ba7
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-03-09 07:13:41 -0700

    Neovim: Switch formatting plugin
    Swap from none-ls to conform. Conform allows the LSP to format if it
    doesn't have a formatter for that language, which non-ls can't do.

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,27 @@
+-- Plugin for formatting
+return {
+	'stevearc/conform.nvim',
+	opts = {},
+	config = function()
+		require("conform").setup({
+			-- Place formatters here
+			-- Note that formatters that come with a language server e.g clang_format
+			-- are detected automatically, so there's no need to include them here
+			formatters_by_ft = {
+			},
+			format_on_save = {
+				lsp_fallback = true,
+				timeout_ms = 500,
+			},
+		})
+		-- Keybind to manually format - comes with a longer timeout
+		vim.keymap.set("n", "<leader>f", function()
+			require("conform").format({
+				lsp_fallback = true,
+				timeout_ms = 1000,
+				async = false,
+			})
+			end
+		)
+	end
+}
