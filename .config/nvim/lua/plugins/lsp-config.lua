58517635c2e36571d9e3f527eff206454f633ff2
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-08-01 05:41:24 -0700

    Fix stuff with neovim config

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -34,7 +34,6 @@ return {
 			
 			local on_attach = function(_, bufnr)
 				vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
-				require('completion').on_attach()
 			end
 
 			lspconfig.clangd.setup({
