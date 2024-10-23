e17f132cfbd3885811bc0dcf2899f1e8d20b62d1
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-10-23 13:41:59 -0700

    Nvim: Reset Trouble config to default - plugin got a rewrite

--- dot_config/nvim/lua/plugins/trouble.lua
+++ dot_config/nvim/lua/plugins/trouble.lua
@@ -1,18 +1,37 @@
 return {
 	"folke/trouble.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	opts = {
-		signs = {
-			-- icons / text used for a diagnostic
-			error = "✕",
-			warning = "⚠",
-			hint = "🗭",
-			information = "🛈",
-			other = "",
+	opts = {}, -- for default options, refer to the configuration section for custom setup.
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics (Trouble)",
 		},
-		fold_open = "v", -- icon used for open folds
-		fold_closed = ">", -- icon used for closed folds
-
-		icons = false,
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics (Trouble)",
+		},
+		{
+			"<leader>cs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols (Trouble)",
+		},
+		{
+			"<leader>cl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ... (Trouble)",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List (Trouble)",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List (Trouble)",
+		},
 	},
 }
