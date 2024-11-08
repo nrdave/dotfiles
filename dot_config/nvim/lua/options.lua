c1fa9770063675b52db89b9e48c407d68c8787f4
Nikhil Dave <nikhilrdave2003@gmail.com>
2024-11-08 15:08:16 -0700

    Small changes in mako, Sway, neovim
    Move all notifications to overlay layer
    Re-add wlr-taskbar in Sway waybar config
    Set scrolloff to 5 in neovim

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -2,6 +2,7 @@ vim.opt.clipboard = 'unnamedplus'   -- use system clipboard
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
 vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 vim.g.mapleader = ','				-- set my leader key to comma
+vim.opt.scrolloff = 5
 
 vim.opt.wildmenu = true
 vim.opt.path:append('**')
